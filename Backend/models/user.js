b67977c8252a3cba9234643cea940daa881b672d
vatsal-prajapati-2002 <vatsal.prajapati@saleshandy.com>
2025-01-31 10:14:08 +0530

    create User Inquiry POST API

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,26 +1,32 @@
-// models/user.js
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    deletedAt: {
+      type: Date,
+      default: null,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  message: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    enum: ["user", "admin"], // Ensure only 'user' or 'admin' can be assigned
-    default: "user", // Default to user role
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
